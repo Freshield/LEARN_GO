ed3bc3c9e286293f8205e3eaffa14e3a5c9c1e2f
freshield <yangyufresh@163.com>
2021-05-20 14:45:10 +0800

    io writer

---
+++ studygolang/chp7_interface/a1_io_writer.go
@@ -0,0 +1,39 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: a1_io_writer.py
+@Time: 2021-10-25 14:43
+@Last_update: 2021-10-25 14:43
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"fmt"
+)
+
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p))
+	return len(p), nil
+}
+
+func main() {
+	var c ByteCounter
+	c.Write([]byte("hello"))
+	fmt.Println(c)
+
+	c = 0
+	var name = "Dolly"
+	fmt.Fprintf(&c, "hello,%c", name)
+	fmt.Println(c)
+
+}
