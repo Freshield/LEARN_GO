8d8e926063db149d51f30b07db702525c580781a
freshield <yangyufresh@163.com>
2021-05-23 14:44:46 +0800

    fetch

---
+++ studygolang/chp5/a8_fetch.go
@@ -0,0 +1,51 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: a8_fetch.py
+@Time: 2021-10-20 16:02
+@Last_update: 2021-10-20 16:02
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"io"
+	"net/http"
+	"path"
+	"os"
+	"fmt"
+)
+
+func fetch(url string) (filename string, n int64, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+	local := path.Base(resp.Request.URL.Path)
+	if local == "/" {
+		local = "index.html"
+	}
+	fmt.Println(local)
+	f, err := os.Create(local)
+	if err != nil {
+		return "", 0, err
+	}
+	n, err = io.Copy(f, resp.Body)
+	if closeErr := f.Close(); err == nil {
+		err = closeErr
+	}
+	return local, n, err
+}
+
+func main() {
+	url := "https://golang.org/"
+	fmt.Println(fetch(url))
+}
