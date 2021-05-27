1f7ea6bd555cf2a3e84e1aab08251dad696c716f
freshield <yangyufresh@163.com>
2021-05-27 19:00:33 +0800

    done server2

---
+++ studygolang/again_chp1/server2.go
@@ -0,0 +1,45 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: server2.py
+@Time: 2021-09-30 17:23
+@Last_update: 2021-09-30 17:23
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler1)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler1(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
