f6a8635e9ddea707a3d899b1c7683346853b11be
freshield <yangyufresh@163.com>
2021-05-28 19:00:17 +0800

    done server1

---
+++ studygolang/again_chp1/server1.go
@@ -0,0 +1,31 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: server1.py
+@Time: 2021-09-30 16:36
+@Last_update: 2021-09-30 16:36
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
