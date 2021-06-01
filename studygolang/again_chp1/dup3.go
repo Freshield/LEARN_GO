d82f83bb388a7fa1afcc833af468d3906291dc19
freshield <yangyufresh@163.com>
2021-06-01 18:59:04 +0800

    done dup3

---
+++ studygolang/again_chp1/dup3.go
@@ -0,0 +1,43 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: dup3.py
+@Time: 2021-09-30 11:33
+@Last_update: 2021-09-30 11:33
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := []string{"test.txt", "test1.txt", "test2.txt"}
+	for _, file_path := range files {
+		data, err := ioutil.ReadFile(file_path)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
