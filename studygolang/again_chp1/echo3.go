9b4d8a181c2dc42f4e8dd9fb92a08c57201e5e3a
freshield <yangyufresh@163.com>
2021-06-04 18:57:57 +0800

    done echo3

---
+++ studygolang/again_chp1/echo3.go
@@ -0,0 +1,26 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: echo3.py
+@Time: 2021-09-29 15:10
+@Last_update: 2021-09-29 15:10
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
