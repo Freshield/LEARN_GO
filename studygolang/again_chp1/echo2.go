285437c155de06627f4b3b11e731fca86c113b4a
freshield <yangyufresh@163.com>
2021-06-26 18:57:31 +0800

    done echo2

---
+++ studygolang/again_chp1/echo2.go
@@ -0,0 +1,30 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: echo2.py
+@Time: 2021-09-29 15:03
+@Last_update: 2021-09-29 15:03
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
