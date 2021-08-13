bc3da1e3cc0558b8d87894b5a853d4e8e08f2c6e
freshield <yangyufresh@163.com>
2021-08-13 18:08:36 +0800

    update

---
+++ studygolang/chp1/a6_dup1.go
@@ -0,0 +1,40 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: test.py
+@Time: 2021-08-15 12:48
+@Last_update: 2021-08-15 12:48
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		if input.Text() != "" {
+			counts[input.Text()]++
+		} else {
+			break
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+
+}
