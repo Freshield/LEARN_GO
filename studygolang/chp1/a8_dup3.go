10e9e5f45964832ca6426804a9c64e359b0c8384
freshield <yangyufresh@163.com>
2021-08-16 18:07:58 +0800

    update

---
+++ studygolang/chp1/a8_dup3.go
@@ -0,0 +1,40 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: a8_dup3.py
+@Time: 2021-08-16 18:00
+@Last_update: 2021-08-16 18:00
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3:%v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		fmt.Printf("%d\t%s\n", n, line)
+	}
+}
