0763d70680094e87844554ed67c53436f8bed591
freshield <yangyufresh@163.com>
2021-08-18 17:59:00 +0800

    begin the search match

---
+++ goinaaction/chp2/a1_search_match/hello_world.go
@@ -0,0 +1,22 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: hello_world.py
+@Time: 2021-08-19 17:57
+@Last_update: 2021-08-19 17:57
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("hello world")
+}
