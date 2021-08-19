bef9659cf322f99d4dec5b3e763773e664ca0b9a
freshield <yangyufresh@163.com>
2021-08-19 18:14:27 +0800

    add search

---
+++ goinaaction/chp2/a1_search_match/main.go
@@ -0,0 +1,31 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: main.py
+@Time: 2021-08-19 18:00
+@Last_update: 2021-08-19 18:00
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	""
+)
+
+func init() {
+	log.SetOutput(os.Stdout)
+}
+
+func main() {
+	fmt.Printf("here")
+}
