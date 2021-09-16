47899e4dc031a729fa65edb161028b75ae90400d
freshield <yangyufresh@163.com>
2021-09-16 11:59:25 +0800

    begin chp6

---
+++ goinaaction/chp6/goroutine.go
@@ -0,0 +1,56 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: goroutine.py
+@Time: 2021-09-16 11:39
+@Last_update: 2021-09-16 11:39
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	runtime.GOMAXPROCS(1)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	fmt.Println("Start Goroutines")
+
+	go func() {
+		defer wg.Done()
+
+		for count := 0; count < 3; count++ {
+			for char := 'a'; char < 'a'+26; char++ {
+				fmt.Printf("%c", char)
+			}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		for count := 0; count < 3; count++ {
+			for char := 'A'; char < 'A'+26; char++ {
+				fmt.Printf("%c", char)
+			}
+		}
+	}()
+
+	fmt.Println("Waiting To Finish")
+	wg.Wait()
+
+	fmt.Println("\nTerminating Program")
+}
