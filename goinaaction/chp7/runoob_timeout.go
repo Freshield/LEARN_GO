d45b3e4ceae22ae59333037ebadbbc05435481cd
freshield <yangyufresh@163.com>
2021-09-19 16:24:33 +0800

    done timeout

---
+++ goinaaction/chp7/runoob_timeout.go
@@ -0,0 +1,37 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: runoob_timeout.py
+@Time: 2021-09-20 16:21
+@Last_update: 2021-09-20 16:21
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func main() {
+	c1 := make(chan string, 1)
+	go func() {
+		time.Sleep(time.Second * 2)
+		c1 <- "result 1"
+	}()
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(time.Second * 10):
+		fmt.Println("timeout 1")
+	}
+}
+
+
