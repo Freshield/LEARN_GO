9db40ecb4b14a24e2672c8e9a26074baee3c3bef
freshield <yangyufresh@163.com>
2021-09-22 17:35:37 +0800

    begin chp7 done runner

---
+++ goinaaction/chp7/runner/runner.go
@@ -0,0 +1,89 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: runner.py
+@Time: 2021-09-22 16:49
+@Last_update: 2021-09-22 16:49
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package runner
+
+import (
+	"errors"
+	"os"
+	"os/signal"
+	"time"
+)
+
+type Runner struct {
+	interrupt chan os.Signal
+
+	complete chan error
+
+	timeout <-chan time.Time
+
+	tasks []func(int)
+}
+
+var ErrTimeout = errors.New("received timeout")
+
+var ErrInterrupt = errors.New("received interrupt")
+
+func New(d time.Duration) *Runner {
+	return &Runner{
+		interrupt: make(chan os.Signal, 1),
+		complete: make(chan error),
+		timeout: time.After(d),
+	}
+}
+
+func (r *Runner) Add(tasks ...func(int)) {
+	r.tasks = append(r.tasks, tasks...)
+}
+
+func (r *Runner) Start() error {
+	signal.Notify(r.interrupt, os.Interrupt)
+
+	go func() {
+		r.complete <- r.run()
+	}()
+
+	select {
+	case err := <-r.complete:
+		return err
+
+	case <-r.timeout:
+		return ErrTimeout
+	}
+}
+
+func (r *Runner) run() error {
+	for id, task := range r.tasks {
+		if r.gotInterrupt() {
+			return ErrInterrupt
+		}
+
+		task(id)
+	}
+
+	return nil
+}
+
+func (r *Runner) gotInterrupt() bool {
+	select {
+	case <-r.interrupt:
+		signal.Stop(r.interrupt)
+		return true
+
+	default:
+		return false
+	}
+}
+
---
+++ goinaaction/chp7/runner_main.go
@@ -0,0 +1,56 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: runner_main.py
+@Time: 2021-09-22 17:27
+@Last_update: 2021-09-22 17:27
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"chp7/runner"
+	"log"
+	"os"
+	"time"
+)
+
+const timeout = 3 * time.Second
+
+func main() {
+	log.Println("Starting work.")
+	
+	r := runner.New(timeout)
+	
+	r.Add(createTask(), createTask(), createTask())
+
+	if err := r.Start(); err != nil {
+		switch err {
+		case runner.ErrTimeout:
+			log.Println("Terminating due to timeout.")
+			os.Exit(1)
+
+		case runner.ErrInterrupt:
+			log.Println("Terminating due to interrupt.")
+			os.Exit(2)
+		}
+
+	}
+
+	log.Println("Process ended.")
+}
+
+func createTask() func(int) {
+	return func(id int) {
+		log.Println("Processor - Task #", id)
+		time.Sleep(time.Duration(id) * time.Second)
+	}
+}
+
