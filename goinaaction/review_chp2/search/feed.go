746552097e863bed400bf5eb93ddcec3abb24e1c
freshield <yangyufresh@163.com>
2021-09-29 11:24:03 +0800

    done feed

---
+++ goinaaction/review_chp2/search/feed.go
@@ -0,0 +1,44 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: feed.py
+@Time: 2021-09-28 16:10
+@Last_update: 2021-09-28 16:10
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package search
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const dataFile = "data/data.json"
+
+type Feed struct {
+	Name string `json:"site"`
+	URI string `json:"link"`
+	Type string `json:"type"`
+}
+
+func RetrieveFeeds() ([]*Feed, error) {
+	file, err := os.Open(dataFile)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	var feeds []*Feed
+	err = json.NewDecoder(file).Decode(&feeds)
+
+	return feeds, err
+}
+
