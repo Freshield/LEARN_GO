bf1a94f114c5d43c109a90f6206e52a9c9c96181
freshield <yangyufresh@163.com>
2021-10-06 14:39:24 +0800

    findlink

---
+++ studygolang/chp5/a1_findlinks1.go
@@ -0,0 +1,55 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: a1_findlinks1.py
+@Time: 2021-10-19 17:33
+@Last_update: 2021-10-19 17:33
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
+	"os"
+)
+
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for  _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
+
+func main() {
+	html_url := "https://golang.org"
+	resp, err := http.Get(html_url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		os.Exit(1)
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
