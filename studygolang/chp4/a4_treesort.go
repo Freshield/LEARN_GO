4de237f817924904cf2ab611ab411150228d1827
freshield <yangyufresh@163.com>
2021-10-08 14:39:07 +0800

    treesort

---
+++ studygolang/chp4/a4_treesort.go
@@ -0,0 +1,60 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: a4_treesort.py
+@Time: 2021-10-13 17:49
+@Last_update: 2021-10-13 17:49
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import "fmt"
+
+type tree struct {
+	value int
+	left, right *tree
+}
+
+func Sort(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
+
+func main() {
+	test := []int{3,2,4,7,6,45,2,7,3}
+	Sort(test)
+	fmt.Println(test)
+}
