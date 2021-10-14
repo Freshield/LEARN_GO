e8296733987e5c2c1c7988a9ef32f80e149dd7bf
freshield <yangyufresh@163.com>
2021-10-14 14:38:17 +0800

    surface

---
+++ studygolang/chp3/a2_surface.go
@@ -0,0 +1,85 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: a2_surface.py
+@Time: 2021-10-09 20:14
+@Last_update: 2021-10-09 20:14
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+)
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+func main() {
+	svgStr := "<html><head><meta http-equiv=\"Countent-Type\" content=\"image/svg+xml\"></head><body>"
+	svgStr += fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+			svgStr += fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	svgStr += fmt.Sprintln("</svg></body></html>")
+	writeToFile(svgStr)
+}
+
+func corner(i, j int) (float64, float64) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// Compute surface height z.
+	z := f(x, y)
+
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // distance from (0,0)
+	return math.Sin(r) / r
+}
+
+func writeToFile(msg string) {
+	f, err := os.OpenFile("test.html", os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer f.Close()
+
+	_, err = f.Write([]byte(msg))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
