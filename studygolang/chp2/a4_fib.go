3a233991f4977b63b2aef04c9980cf81f6a16acc
freshield <yangyufresh@163.com>
2021-10-19 14:37:05 +0800

    fib

---
+++ studygolang/chp2/a4_fib.go
@@ -0,0 +1,30 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: a4_fib.py
+@Time: 2021-10-09 14:47
+@Last_update: 2021-10-09 14:47
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import "fmt"
+
+func fib(n int) int {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		x, y = y, x+y
+	}
+	return x
+}
+
+func main() {
+	fmt.Println(fib(10))
+}
