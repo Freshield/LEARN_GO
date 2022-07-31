9352a8113798f6641d7d1946e94b8b767ad5d37b
freshield <yangyufresh@163.com>
2022-07-31 14:58:08 +0800

    done match

---
+++ goinaaction/chp2/a1_search_match/search/match.go
@@ -0,0 +1,48 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: match.py
+@Time: 2021-08-22 01:48
+@Last_update: 2021-08-22 01:48
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package search
+
+import (
+	"fmt"
+	"log"
+)
+
+type Result struct {
+	Field string
+	Content string
+}
+
+type Matcher interface {
+	Search(feed *Feed, searchTerm string) ([]*Result, error)
+}
+
+func Match(matcher Matcher, feed *Feed, searchTerm string, results chan<- *Result) {
+	searchResults, err := matcher.Search(feed, searchTerm)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for _, result := range searchResults {
+		results <- result
+	}
+}
+
+func Display(results chan *Result) {
+	for result := range results {
+		fmt.Printf("%s:\n%s\n\n", result.Field, result.Content)
+	}
+}
